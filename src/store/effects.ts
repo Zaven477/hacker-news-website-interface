8052cebdaf25738dd39685bdbbafbbef25d4c1a5
Zaven477 <papikyan86@bk.ru>
2023-03-14 16:55:10 +0400

    data loading tracking and error handling

--- src/store/effects.ts
+++ src/store/effects.ts
@@ -1,15 +1,24 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getNews, getNewsById } from "../api";
-import { setNews } from "./newsSlice";
+import { setErrors, setLoading, setNews } from "./newsSlice";
 
 export const fetchNews = createAsyncThunk("news", async (_, { dispatch }) => {
-  const response = await getNews();
+  try {
+    dispatch(setLoading(true));
+    const response = await getNews();
 
-  const promises = response.map(async (id: number) => {
-    const result = await getNewsById(id);
-    return result.data;
-  });
+    const promises = response.map(async (id: number) => {
+      const result = await getNewsById(id);
+      return result.data;
+    });
 
-  const news = await Promise.all(promises);
-  dispatch(setNews(news));
+    const news = await Promise.all(promises);
+    dispatch(setNews(news));
+    dispatch(setLoading(false));
+  } catch (error) {
+    if (error instanceof Error) {
+      dispatch(setLoading(false));
+      dispatch(setErrors(error.message));
+    }
+  }
 });
